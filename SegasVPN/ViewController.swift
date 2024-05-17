6f4da9fff8ba887249a70f28f1a9e9d39a42b42e
hassan <hasan@digilite.ca>
2024-05-17 09:04:18 +0400

    Initial Commit

---
+++ SegasVPN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegasVPN
+//
+//  Created by macbook on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
